a7b3c595ed01965b45bc1cd25a4afa8ee29121a8
deadend1415 <ahipuedaniels@gmail.com>
2025-10-30 22:36:58 +0100

    auto hook config

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+read -p "Do you want to install the git hooks? (y/n): " choice
+if [[ "$choice" == [Yy] ]]; then
+  mv pre-comm/hooks
+fi
+echo "If you dont intend on checking syntax for a language you can skip the dependecy"
+sleep 5
 sudo apt install shellcheck yamllint php node python3
